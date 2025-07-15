ffe9597283bc1f1efce5db7747ec9803d3fe2f73
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2025-07-15 14:23:12 -0500

    upload docs as github artifact

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -45,7 +45,9 @@ rapids-print-env
 
 rapids-logger "Build Docs"
 ./build.sh docs
-mkdir -p "${RAPIDS_DOCS_DIR}/cuopt/html"
-mv docs/cuopt/build/html/* "${RAPIDS_DOCS_DIR}/cuopt/html"
+mkdir -p "${RAPIDS_DOCS_DIR}/cuopt"
+zip -r /tmp/cuopt_docs.zip docs/cuopt/build/html/*
+mv /tmp/cuopt_docs.zip "${RAPIDS_DOCS_DIR}/cuopt/cuopt_docs.zip"
+
 
 
