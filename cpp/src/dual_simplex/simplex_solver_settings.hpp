0e14fe2fb2f2fc2f31a9849e6859110b3326c98b
nicolas <nguidotti@nvidia.com>
2025-09-23 17:45:23 +0200

    fixed initialization order

--- cpp/src/dual_simplex/simplex_solver_settings.hpp
+++ cpp/src/dual_simplex/simplex_solver_settings.hpp
@@ -97,15 +97,17 @@ struct simplex_solver_settings_t {
   bool use_bound_flip_ratio;       // true if using the bound flip ratio test
   bool scale_columns;              // true to scale the columns of A
   bool relaxation;                 // true to only solve the LP relaxation of a MIP
-  bool
-    use_left_looking_lu;  // true to use left looking LU factorization, false to use right looking
-  bool eliminate_singletons;    // true to eliminate singletons from the basis
-  bool print_presolve_stats;    // true to print presolve stats
-  i_t refactor_frequency;       // number of basis updates before refactorization
-  i_t iteration_log_frequency;  // number of iterations between log updates
-  i_t first_iteration_log;      // number of iterations to log at beginning of solve
-  i_t num_threads;              // number of threads to use
-  i_t random_seed;              // random seed
+  bool use_left_looking_lu;        // true to use left looking LU factorization,
+                                   // false to use right looking
+  bool eliminate_singletons;       // true to eliminate singletons from the basis
+  bool print_presolve_stats;       // true to print presolve stats
+  i_t refactor_frequency;          // number of basis updates before refactorization
+  i_t iteration_log_frequency;     // number of iterations between log updates
+  i_t first_iteration_log;         // number of iterations to log at beginning of solve
+  i_t num_threads;                 // number of threads to use
+  i_t num_bfs_threads;             // number of threads dedicated to the best-first search
+  i_t num_diving_threads;          // number of threads dedicated to diving
+  i_t random_seed;                 // random seed
   i_t inside_mip;  // 0 if outside MIP, 1 if inside MIP at root node, 2 if inside MIP at leaf node
   std::function<void(std::vector<f_t>&, f_t)> solution_callback;
   std::function<void()> heuristic_preemption_callback;
@@ -113,9 +115,6 @@ struct simplex_solver_settings_t {
   mutable logger_t log;
   std::atomic<i_t>* concurrent_halt;  // if nullptr ignored, if !nullptr, 0 if solver should
                                       // continue, 1 if solver should halt
-
-  i_t num_bfs_threads;
-  i_t num_diving_threads;
 };
 
 }  // namespace cuopt::linear_programming::dual_simplex
