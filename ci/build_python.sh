dbaf523fa93315aba8acb6eb9cb0438e93fae24d
James Lamb <jlamb@nvidia.com>
2026-08-07 11:15:33 -0500

    ensure nightly builds always produce new packages (#1661)
    Contributes to https://github.com/rapidsai/build-planning/issues/218
    
    * uses the new patterns for versioning nightly packages (see https://github.com/rapidsai/shared-workflows/pull/603), to ensure they're always uploaded on new builds. 
    
    For wheels:
    
    ```text
    # before
    26.10.0a55 
    
    # after
    26.10.0a55.post260803200854
    ```
    
    And for conda:
    
    ```text
    # before
    26.10.0a55[build=cuda12_260803_9da146ef]
    
    # after
    26.10.0a55[build=cuda12_260803200854_9da146ef]
    ```
    
    ## Notes for Reviewers
    
    ### How I tested this
    
    See https://github.com/rapidsai/cugraph-gnn/pull/508
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Ramakrishna Prabhu (https://github.com/ramakrishnap-nv)
    
    URL: https://github.com/NVIDIA/cuopt/pull/1661

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 
 source rapids-configure-sccache
 
-source rapids-date-string
+source rapids-datetime-string
 
 export CMAKE_GENERATOR=Ninja
 
--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -9,11 +9,12 @@ package_name=$1
 package_dir=$2
 
 source rapids-configure-sccache
-source rapids-date-string
+source rapids-datetime-string
 source rapids-init-pip
 
-# Update the version to accomdate nightly and release changes for the wheel name
-rapids-generate-version > ./VERSION
+# Update the version to accomodate nightly and release changes for the wheel name
+RAPIDS_VERSION_SUFFIX=".post${RAPIDS_DATETIME_STRING}" \
+  rapids-generate-version > ./VERSION
 
 cd "${package_dir}"
 
