c1f55ea1206adfe011b28a5c9984cf6369b18f1f
Vitor Sessak <vsessak@google.com>
2026-09-17 00:14:44 +0200

    Guard against nullptr dereference when checking concurrent solver status (#1845)
    ## Issue
    
    Authors:
      - Vitor Sessak (https://github.com/vitor1001)
      - Hugo Linsenmaier (https://github.com/hlinsen)
    
    Approvers:
      - Hugo Linsenmaier (https://github.com/hlinsen)
    
    URL: https://github.com/NVIDIA/cuopt/pull/1845

--- cpp/src/pdlp/solve.cu
+++ cpp/src/pdlp/solve.cu
@@ -1559,6 +1559,9 @@ void run_dual_simplex_thread(
     run_dual_simplex(problem, settings, timer));
 }
 
+/**
+ * @brief Solve an LP problem using concurrent solvers (PDLP, dual simplex, barrier).
+ */
 template <typename i_t, typename f_t>
 optimization_problem_solution_t<i_t, f_t> run_concurrent(
   mip::problem_t<i_t, f_t>& problem,
@@ -1737,10 +1740,12 @@ optimization_problem_solution_t<i_t, f_t> run_concurrent(
   f_t end_time = timer.elapsed_time();
   CUOPT_LOG_CONDITIONAL_INFO(!settings.inside_mip, "Concurrent time: %.3fs", end_time);
 
-  const auto dual_simplex_status = !settings.inside_mip ? std::get<1>(*sol_dual_simplex_ptr)
-                                                        : simplex::lp_status_t::CONCURRENT_LIMIT;
-  const auto barrier_status =
-    enable_barrier ? std::get<1>(*sol_barrier_ptr) : simplex::lp_status_t::CONCURRENT_LIMIT;
+  const auto dual_simplex_status = (!settings.inside_mip && sol_dual_simplex_ptr != nullptr)
+                                     ? std::get<1>(*sol_dual_simplex_ptr)
+                                     : simplex::lp_status_t::CONCURRENT_LIMIT;
+  const auto barrier_status      = (enable_barrier && sol_barrier_ptr != nullptr)
+                                     ? std::get<1>(*sol_barrier_ptr)
+                                     : simplex::lp_status_t::CONCURRENT_LIMIT;
   const bool dual_simplex_solved = dual_simplex_status == simplex::lp_status_t::OPTIMAL ||
                                    dual_simplex_status == simplex::lp_status_t::INFEASIBLE ||
                                    dual_simplex_status == simplex::lp_status_t::UNBOUNDED;
@@ -1815,7 +1820,8 @@ optimization_problem_solution_t<i_t, f_t> run_concurrent(
     CUOPT_LOG_CONDITIONAL_INFO(!settings.inside_mip, "Solved with PDLP");
     return sol_pdlp;
   } else if (!settings.inside_mip &&
-             sol_pdlp.get_termination_status() == pdlp_termination_status_t::ConcurrentLimit) {
+             sol_pdlp.get_termination_status() == pdlp_termination_status_t::ConcurrentLimit &&
+             sol_dual_simplex_ptr != nullptr) {
     sol_barrier_ptr.reset();
     auto& dual_simplex_solution = std::get<0>(*sol_dual_simplex_ptr);
     auto sol_dual_simplex       = convert_dual_simplex_sol(problem,
--- cpp/tests/linear_programming/pdlp_test.cu
+++ cpp/tests/linear_programming/pdlp_test.cu
@@ -33,6 +33,7 @@
 
 #include <utilities/copy_helpers.hpp>
 #include <utilities/error.hpp>
+#include <utilities/scope_guard.hpp>
 
 #include <raft/sparse/detail/cusparse_wrappers.h>
 #include <raft/core/cusparse_macros.hpp>
@@ -55,6 +56,8 @@
 #include <tuple>
 #include <utility>
 #include <vector>
+
+#include <omp.h>
 
 namespace cuopt::mathematical_optimization::test {
 
@@ -172,6 +175,55 @@ TEST(pdlp_class, concurrent_pdlp_exception_joins_worker_threads)
   EXPECT_EQ(error_status.get_error_type(), cuopt::error_type_t::ValidationError);
   EXPECT_THAT(error_status.what(),
               testing::HasSubstr("all_primal_feasible only applies in batch mode"));
+}
+
+TEST(pdlp_class, concurrent_null_solver_ptrs_inside_mip)
+{
+  const raft::handle_t handle_{};
+
+  auto path = make_path_absolute("linear_programming/afiro_original.mps");
+  cuopt::mathematical_optimization::io::mps_data_model_t<int, double> op_problem =
+    cuopt::mathematical_optimization::io::read_mps<int, double>(path, true);
+
+  auto settings       = pdlp_solver_settings_t<int, double>{};
+  settings.method     = cuopt::mathematical_optimization::method_t::Concurrent;
+  settings.presolver  = cuopt::mathematical_optimization::presolver_t::None;
+  settings.inside_mip = true;
+
+  // inside_mip skips dual simplex. Setting threads to 1 ensures barrier is also disabled
+  // (< CUOPT_CONCURRENT_LP_BARRIER_REQUIRED_THREAD_COUNT), leaving both sol_dual_simplex_ptr
+  // and sol_barrier_ptr null.
+  const int prev_threads = omp_get_max_threads();
+  omp_set_num_threads(1);
+  const cuopt::scope_guard restore_threads{[prev_threads] { omp_set_num_threads(prev_threads); }};
+  optimization_problem_solution_t<int, double> solution = solve_lp(&handle_, op_problem, settings);
+
+  EXPECT_EQ((int)solution.get_termination_status(), CUOPT_TERMINATION_STATUS_OPTIMAL);
+}
+
+TEST(pdlp_class, concurrent_null_dual_simplex_concurrent_limit)
+{
+  const raft::handle_t handle_{};
+
+  auto path = make_path_absolute("linear_programming/afiro_original.mps");
+  cuopt::mathematical_optimization::io::mps_data_model_t<int, double> op_problem =
+    cuopt::mathematical_optimization::io::read_mps<int, double>(path, true);
+
+  auto settings       = pdlp_solver_settings_t<int, double>{};
+  settings.method     = cuopt::mathematical_optimization::method_t::Concurrent;
+  settings.presolver  = cuopt::mathematical_optimization::presolver_t::None;
+  settings.inside_mip = true;
+
+  // With inside_mip = true, dual simplex is not started (sol_dual_simplex_ptr is null).
+  // Pre-setting concurrent_halt causes PDLP to immediately exit with ConcurrentLimit.
+  // The solver must return PDLP's result without attempting to dereference
+  // sol_dual_simplex_ptr.
+  std::atomic<int> halt{1};
+  settings.concurrent_halt = &halt;
+
+  optimization_problem_solution_t<int, double> solution = solve_lp(&handle_, op_problem, settings);
+  EXPECT_EQ(solution.get_termination_status(),
+            cuopt::mathematical_optimization::pdlp_termination_status_t::ConcurrentLimit);
 }
 
 TEST(pdlp_class, run_double_very_low_accuracy)
